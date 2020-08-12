8c33eaa78642004e03df69900743b9f1471b3a1c
Jonathan Salwan <jonathan.salwan@gmail.com>
2020-08-12 14:48:51 +0200

    remove files

--- tritondse/__init__.py
+++ tritondse/__init__.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from .config                import Config
-from .enums                 import *
-from .processState          import ProcessState
-from .loaders               import ELFLoader
-from .program               import Program
-from .seed                  import Seed
-from .symbolicExecutor      import SymbolicExecutor
-from .threadContext         import ThreadContext
-from .routines              import *
+from .config           import Config
+from .processState     import ProcessState
+from .loaders          import ELFLoader
+from .program          import Program
+from .seed             import Seed
+from .symbolicExecutor import SymbolicExecutor
+from .threadContext    import ThreadContext
+from .routines         import *
--- tritondse/enums.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from enum import Enum, auto
-
-
