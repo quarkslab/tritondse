eaef055d4c72e25bb52953a8e5beb7baf30cf550
Jonathan Salwan <jonathan.salwan@gmail.com>
2020-08-25 14:38:55 +0200

    Fix space comment

--- tritondse/symbolic_executor.py
+++ tritondse/symbolic_executor.py
@@ -124,7 +124,7 @@ class SymbolicExecutor(object):
             # Simulate that the time of an executed instruction is time_inc_coefficient.
             # For example, if time_inc_coefficient is 0.0001, it means that an instruction
             # takes 100us to be executed. Used to provide a deterministic behavior when
-            #calling time functions (e.g gettimeofday(), clock_gettime(), ...).
+            # calling time functions (e.g gettimeofday(), clock_gettime(), ...).
             self.pstate.time += self.config.time_inc_coefficient
 
             # Update the coverage of the execution
