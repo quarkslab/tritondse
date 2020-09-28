3a5e396be187d04e67907269573701b0e53dad27
Jonathan Salwan <jonathan.salwan@gmail.com>
2020-09-28 15:28:01 +0200

    Add a setup script

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Installation of the tritondse module."""
+
+import sys
+from setuptools import setup, find_packages
+
+setup(
+    name="tritondse",
+    version="0.1",
+    description="A library of Dynamic Symbolic Exploration based the Triton library",
+    packages=find_packages(),
+    setup_requires=[],
+    install_requires=["lief"],
+    tests_require=[],
+    license="qb",
+    author="Quarkslab",
+    classifiers=[
+        'Topic :: Security',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+    ],
+    test_suite="",
+    scripts=[]
+)
