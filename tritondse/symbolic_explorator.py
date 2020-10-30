05fa507d098f27e3e90de58c6a4c6106cd2bcd12
Robin David <rdavid@quarkslab.com>
2020-10-30 15:29:13 +0100

    fix KeyboardInterrup + add status in return of explore

--- tritondse/symbolic_explorator.py
+++ tritondse/symbolic_explorator.py
@@ -2,16 +2,23 @@ import logging
 import time
 import threading
 import gc
+from enum import Enum
 
-from triton                      import *
 from tritondse.config            import Config
 from tritondse.process_state     import ProcessState
 from tritondse.program           import Program
 from tritondse.seed              import Seed, SeedFile
 from tritondse.seeds_manager     import SeedsManager
-from tritondse.enums             import Enums
 from tritondse.symbolic_executor import SymbolicExecutor
 from tritondse.callbacks         import CallbackManager
+
+
+class ExplorationStatus(Enum):
+    """ Enum representing the current state of the exploration """
+    NOT_RUNNING = 0
+    RUNNING     = 1
+    IDLE        = 2
+    STOPPED     = 3
 
 
 class SymbolicExplorator(object):
@@ -23,9 +30,10 @@ class SymbolicExplorator(object):
         self.config        = config
         self.cbm           = CallbackManager(program)
         self.seeds_manager = SeedsManager(self.config, self.cbm)
-        self.stop          = False
+        self._stop          = False
         self.ts            = time.time()
         self.uid_counter   = 0
+        self.status = ExplorationStatus.NOT_RUNNING
 
 
     @property
@@ -43,7 +51,7 @@ class SymbolicExplorator(object):
 
         if self.config.exploration_timeout and self.__time_delta() >= self.config.exploration_timeout:
             logging.info('Exploration timout')
-            self.stop = True
+            self._stop = True
             return
 
         # Execute the binary with seeds
@@ -53,7 +61,7 @@ class SymbolicExplorator(object):
 
         if self.config.exploration_limit and (uid+1) >= self.config.exploration_limit:
             logging.info('Exploration limit reached')
-            self.stop = True
+            self._stop = True
             return
 
         # Some analysis in post execution
@@ -62,28 +70,43 @@ class SymbolicExplorator(object):
         logging.info('Total time of the exploration: %f seconds' % (self.__time_delta()))
 
 
-    def explore(self):
-        while self.seeds_manager.worklist and not self.stop:
+    def explore(self) -> ExplorationStatus:
+        self.status = ExplorationStatus.RUNNING
 
-            # Take an input
-            seed = self.seeds_manager.pick_seed()
+        try:
+            while self.seeds_manager.worklist and not self._stop:
+                # Take an input
+                seed = self.seeds_manager.pick_seed()
 
-            # Execution into a thread
-            t = threading.Thread(
-                    name='\033[0;%dm[exec:%08d]' % ((31 + (self.uid_counter % 4)), self.uid_counter),
-                    target=self.worker,
-                    args=[seed, self.uid_counter],
-                    daemon=True
-                )
-            t.start()
-            self.uid_counter += 1
+                # Execution into a thread
+                t = threading.Thread(
+                        name='\033[0;%dm[exec:%08d]' % ((31 + (self.uid_counter % 4)), self.uid_counter),
+                        target=self.worker,
+                        args=[seed, self.uid_counter],
+                        daemon=True
+                    )
+                t.start()
+                self.uid_counter += 1
 
-            try:
                 while True:
                     t.join(0.001)
                     if not t.is_alive():
                         break
                 gc.collect()  # FIXME: Why we have to force the collect to avoid memory leak?
-            except KeyboardInterrupt:
-                logging.warning("keyboard interrupt, stop symbolic exploration")
-                self.stop = True
+
+            # Exited loop
+            self.status = ExplorationStatus.STOPPED if self._stop else ExplorationStatus.IDLE
+
+        except KeyboardInterrupt:
+            logging.warning("keyboard interrupt, stop symbolic exploration")
+            self.status = ExplorationStatus.STOPPED
+
+        return self.status
+
+    def add_input_seed(self, data: bytes) -> None:
+        """ Add the given bytes as input for the exploration """
+        self.seeds_manager.add_seed(Seed(data))
+
+    def stop_exploration(self) -> None:
+        """ Interrupt exploration """
+        self._stop = True
