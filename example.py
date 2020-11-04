a064288a325785a1a4ff589275841d87135c652f
RobinDavid <dev.robin.david@gmail.com>
2020-11-04 11:23:00 +0100

    ajustement pour example.py

--- example.py
+++ example.py
@@ -19,12 +19,12 @@ config.coverage_strategy    = CoverageStrategy.EDGE_COVERAGE
 config.symbolize_stdin      = True
 config.program_argv         = [
     #b'../aarch64/micro_http_server_tt_fuzz_single_without_vuln',
-    b'../programme_etalon_final/micro_http_server/micro_http_server_tt_fuzz_single_with_vuln',
-    b'wlp0s20f3',
-    b'48:e2:44:f5:9b:01',
-    b'10.0.13.86',
-    b'255.255.255.0',
-    b'10.0.13.254'
+    '../programme_etalon_final/micro_http_server/micro_http_server_tt_fuzz_single_with_vuln',
+    'wlp0s20f3',
+    '48:e2:44:f5:9b:01',
+    '10.0.13.86',
+    '255.255.255.0',
+    '10.0.13.254'
     #b'/home/jonathan/Works/QuarksLab/Missions/pastis/programme_etalon_etatique/bin/target'
 ]
 
@@ -36,14 +36,14 @@ except FileNotFoundError as e:
     print(e)
     sys.exit(-1)
 
-seed = SeedFile('../programme_etalon_final/micro_http_server/misc/frame.seed')
+seed = Seed.from_file('../programme_etalon_final/micro_http_server/misc/frame.seed')
 #seed = SeedFile('/home/jonathan/Works/QuarksLab/Missions/pastis/programme_etalon_etatique/fuzzing/in/tcp_echo_1')
 #seed = SeedFile('/home/jonathan/Works/QuarksLab/Missions/pastis/programme_etalon_etatique/fuzzing/in/frame_ip4_tcp_syn')
 
 #dse = SymbolicExplorator(config, program, seed)
 #dse.explore()
 
-ps = ProcessState(config)
+ps = ProcessState(config.thread_scheduling, config.time_inc_coefficient)
 execution = SymbolicExecutor(config, ps, program, seed)
 #execution.callback_manager.register_post_instuction_callback(trace_debug)
 execution.run()
