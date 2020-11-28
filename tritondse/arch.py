724876dc1bbeda60b9fcc6f4b09e3378a7c2b723
Robin David <rdavid@quarkslab.com>
2020-11-28 23:08:17 +0100

    fix dirty hlt now use OPCODE

--- tritondse/arch.py
+++ tritondse/arch.py
@@ -1,12 +1,13 @@
 from collections     import namedtuple
 from tritondse.types import Architecture, Addr
+from triton          import OPCODE
 
 Arch = namedtuple("Arch", "ret_reg pc_reg bp_reg sp_reg sys_reg reg_args halt_inst")
 
 ARCHS = {
-    Architecture.X86:     Arch('eax', 'eip', 'ebp', 'esp', 'eax', [], b"\xf4"),                                         # FIXME: Use OPCODE.X86.HLT
-    Architecture.X86_64:  Arch('rax', 'rip', 'rbp', 'rsp', 'rax', ['rdi', 'rsi', 'rdx', 'rcx', 'r8', 'r9'], b"\xf4"),   # FIXME: Use OPCODE.X86.HLT
-    Architecture.AARCH64: Arch('x0', 'pc', 'sp', 'sp', 'x8', ['x0', 'x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7'], b"")     # FIXME: Use OPCODE.AARCH64.HLT
+    Architecture.X86:     Arch('eax', 'eip', 'ebp', 'esp', 'eax', [], OPCODE.X86.HLT),
+    Architecture.X86_64:  Arch('rax', 'rip', 'rbp', 'rsp', 'rax', ['rdi', 'rsi', 'rdx', 'rcx', 'r8', 'r9'], OPCODE.X86.HLT),
+    Architecture.AARCH64: Arch('x0', 'pc', 'sp', 'sp', 'x8', ['x0', 'x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7'], OPCODE.AARCH64.HLT)
     # ARM ?
 }
 
