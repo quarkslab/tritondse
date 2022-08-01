cb9eb3fdc2f1e354db819fd85c15f72bf230e26a
RobinDavid <dev.robin.david@gmail.com>
2022-08-01 14:31:27 +0200

    update json parser solution

--- doc/practicals/solution_json_parser.py
+++ doc/practicals/solution_json_parser.py
@@ -1,47 +1,47 @@
 #!/usr/bin/env python
-from tritondse import ProbeInterface, SymbolicExecutor, Config, Program, SymbolicExplorator, ProcessState, CbType, SeedStatus, Seed, SeedFormat, Loader, MonolithicLoader, CompositeData, CompositeField
-from tritondse.types import Addr, SolverStatus, Architecture
+from tritondse import SymbolicExecutor, Config, Program, SymbolicExplorator, ProcessState, Workspace, Seed, \
+                      SeedFormat, Loader, MonolithicLoader, CompositeData, CompositeField, Addr, SolverStatus, Architecture, \
+                      LoadableSegment, Perm, SeedStatus, SmtSolver
 from tritondse.sanitizers import NullDerefSanitizer
-from triton import Instruction
-from pwn import *
+import struct
+
 import logging
-#logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
-cnt = 0
-n_inst = 0
-LIGHTHOUSE = True
-TRACE_FOLDER = "./lighthouse/"
+# Memory mapping
+STACK_ADDR  = 0x1000000
+STACK_SIZE  = 1024*6
+STRUC_ADDR  = 0x3000000
+BUFFER_ADDR = 0x2000000
+BASE_ADDRESS= 0x8000000
+USER_CB     = 0x40000000
 
-def trace_inst(exec: SymbolicExecutor, pstate: ProcessState, inst: Instruction):
-    if LIGHTHOUSE: 
-        with open(TRACE_FOLDER + str(exec.seed.hash) + ".trace", "a") as fd:
-            fd.write(f"0x{inst.getAddress():x}\n")
-    global n_inst
-    if n_inst > 50000:
-        print("Executed more than 50000 instructions")
-        exec.seed.status = SeedStatus.CRASH
-        pstate.stop = True
-    n_inst += 1
+# Addresses
+ENTRY_POINT = 0x81dc46e
+EXIT_POINT  = 0x81dc472
+STUB_ADDR1  = 0x81d1bf0
+STUB_ADDR2  = 0x81d1252
+
+
+def pre_inst(se: SymbolicExecutor, state: ProcessState, inst):
+    if se.trace_offset > 40000:
+        se.seed.status = SeedStatus.HANG
+        se.abort()
+    #print(f"[{se.trace_offset}] {inst.getAddress():#08x}: {inst.getDisassembly()}")
 
 def post_exec_hook(se: SymbolicExecutor, state: ProcessState):
-    global cnt
-    cnt += 1
-    print(f"{cnt}. : seed:{se.seed.hash} ({repr(se.seed.content.variables['buffer'])})   [exitcode:{se.exitcode}]")
-
-def pre_exec_hook(se: SymbolicExecutor, state: ProcessState):
-    global n_inst 
-    n_inst = 0
+    print(f"[{se.uid}] seed:{se.seed.hash} ({repr(se.seed.content.variables['buffer'][:100])}) => {se.seed.status.name}   [exitcode:{se.exitcode}]")
 
 def fptr_stub(exec: SymbolicExecutor, pstate: ProcessState, addr: int):
     print(f"fptr_stub addr : {addr:#x}")
-    if addr == 0x81d1bf0:
+    if addr == STUB_ADDR1:
         pstate.cpu.r0 = 1
-    elif addr == 0x81d1252:
+    elif addr == STUB_ADDR2:
         pstate.cpu.r0 = 0
     pstate.cpu.program_counter += 2
     exec.skip_instruction()
 
-def hook_start(exec: SymbolicExecutor, pstate: ProcessState, addr: int):
+def hook_start(exec: SymbolicExecutor, pstate: ProcessState):
     buffer = pstate.get_argument_value(0)
     length = pstate.get_argument_value(1)
     JSON_ctx = pstate.get_argument_value(2)
@@ -51,37 +51,57 @@ def hook_start(exec: SymbolicExecutor, pstate: ProcessState, addr: int):
     exec.inject_symbolic_input(JSON_ctx, exec.seed.content.variables,\
             var_prefix="JSON_ctx", compfield=CompositeField.VARIABLE)
 
-    # We hardcode the length
-    pstate.cpu.r1 = 128
+    # Take the length of the buffer (which is not meant to change)
+    pstate.cpu.r1 = len(exec.seed.content.variables['buffer'])
 
-conf = Config(skip_unsupported_import=True,\
-        seed_format=SeedFormat.COMPOSITE)
+conf = Config(skip_unsupported_import=True, seed_format=SeedFormat.COMPOSITE, smt_solver=SmtSolver.Z3)
 
-ldr = MonolithicLoader(\
-        "./bugged_json_parser.bin", \
-        Architecture.ARM32, \
-        0x8000000,\
-        cpustate = {"pc": 0x81dc46e, \
-                    "r0": 0x800000, \
-                    "r2": 0x700000}, \
-        set_thumb=True,\
-        vmmap = { 0x800000 : b"\x00"*128,\
-                0x700000 : b"\x00"*512,\
-                0x600000 : b"\x00"*0x1000})
+ldr = MonolithicLoader("./bugged_json_parser.bin", Architecture.ARM32, BASE_ADDRESS,
+                       perm = Perm.R | Perm.X,
+                       cpustate = {"pc": ENTRY_POINT, 
+                                   "r0": BUFFER_ADDR,
+                                   "r2": STRUC_ADDR,
+                                   "sp": STACK_ADDR+STACK_SIZE},
+                       set_thumb=True,
+                       vmmap = [LoadableSegment(BUFFER_ADDR, 40, Perm.R|Perm.W, name="input"),
+                                LoadableSegment(STRUC_ADDR, 512, Perm.R|Perm.W, name="JSON_ctx"),
+                                LoadableSegment(USER_CB, 1000, Perm.R|Perm.X, name="user_cb"),
+                                LoadableSegment(STACK_ADDR, STACK_SIZE, Perm.R|Perm.W, name="[stack]")])
 
-dse = SymbolicExplorator(conf,\
-        ldr, executor_stop_at=0x81dc472)
+workspace = Workspace("ws")
 
-membuf_addr = 0x600000
-composite_data = CompositeData(variables={"buffer" : b"A"*128, "JSON_ctx": p32(membuf_addr)*128})
-dse.add_input_seed(Seed(composite_data))
+dse = SymbolicExplorator(conf, ldr, executor_stop_at=EXIT_POINT, workspace=workspace)
 
-dse.callback_manager.register_probe(NullDerefSanitizer())
+seed = Seed(CompositeData(variables={
+                                    "buffer" : b"A"*40,
+                                    "JSON_ctx": b"\x00"*128}))#struct.pack("<I", USER_CB)*128}))
+dse.add_input_seed(seed)
+
+# dse.callback_manager.register_probe(NullDerefSanitizer())
 dse.callback_manager.register_post_execution_callback(post_exec_hook)
-dse.callback_manager.register_pre_execution_callback(pre_exec_hook)
-dse.callback_manager.register_post_instruction_callback(trace_inst)
-dse.callback_manager.register_pre_addr_callback(0x81d140e, hook_start)
-dse.callback_manager.register_pre_addr_callback(0x81d1bf0, fptr_stub)
-dse.callback_manager.register_pre_addr_callback(0x81d1252, fptr_stub)
+dse.callback_manager.register_pre_execution_callback(hook_start)
+
+dse.callback_manager.register_pre_instruction_callback(pre_inst)
+
+dse.callback_manager.register_pre_addr_callback(STUB_ADDR1, fptr_stub)
+dse.callback_manager.register_pre_addr_callback(STUB_ADDR2, fptr_stub)
 
 dse.explore()
+
+# seed = Seed(CompositeData(variables={
+#                                     "buffer" : b"A"*40,
+#                                     "JSON_ctx": b"\x00"*128}))#struct.pack("<I", USER_CB)*128}))
+
+# dse = SymbolicExecutor(conf, seed, workspace=workspace)
+# dse.load(ldr)
+
+# # dse.callback_manager.register_probe(NullDerefSanitizer())
+# dse.cbm.register_post_execution_callback(post_exec_hook)
+# dse.cbm.register_pre_execution_callback(hook_start)
+
+# dse.cbm.register_pre_instruction_callback(pre_inst)
+
+# dse.cbm.register_pre_addr_callback(STUB_ADDR1, fptr_stub)
+# dse.cbm.register_pre_addr_callback(STUB_ADDR2, fptr_stub)
+
+# dse.run(stop_at=EXIT_POINT)
