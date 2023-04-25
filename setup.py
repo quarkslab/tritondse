e768dbbdc8be8514e6a8b2ce193b974c169c19fa
Robin David <dev.robin.david@gmail.com>
2023-04-25 07:13:57 +0200

    update setup.py info for v0.1.2

--- setup.py
+++ setup.py
@@ -7,11 +7,23 @@ import sys
 from setuptools import setup, find_packages
 
 
+with open("README.md") as f:
+    README = f.read()
+
+
 setup(
     name="tritondse",
     version="0.1.2",
     description="A library of Dynamic Symbolic Exploration based the Triton library",
     packages=find_packages(),
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url="https://github.com/quarkslab/tritondse",
+    project_urls={
+        "Documentation": "https://quarkslab.github.io/tritondse/",
+        "Bug Tracker": "https://github.com/quarkslab/tritondse/issues",
+        "Source": "https://github.com/quarkslab/tritondse"
+    },
     setup_requires=[],
     install_requires=[
         "triton-library",
@@ -22,7 +34,7 @@ setup(
         "enum_tools"
     ],
     tests_require=[],
-    license="qb",
+    license="AGPL-3.0",
     author="Quarkslab",
     classifiers=[
         'Topic :: Security',
