9f5efd372c9a143757f8a79d9a397f21613d7981
Christian Heitman <cheitman@quarkslab.com>
2024-05-08 13:43:38 -0300

    Refactor solve1 script

--- doc/practicals/solutions_toy_examples/solve1.py
+++ doc/practicals/solutions_toy_examples/solve1.py
@@ -1,30 +1,42 @@
-from tritondse import ProbeInterface, SymbolicExecutor, Config, Program, SymbolicExplorator, ProcessState, CbType, SeedStatus, Seed, SeedFormat, CompositeData
-from tritondse.types import Addr, SolverStatus, Architecture
+import logging
+
+from triton import Instruction
+
+from tritondse.config import Config
+from tritondse.loaders.program import Program
+from tritondse.process_state import ProcessState
 from tritondse.sanitizers import NullDerefSanitizer
+from tritondse.seed import CompositeData
+from tritondse.seed import Seed
+from tritondse.seed import SeedFormat
+from tritondse.symbolic_executor import SymbolicExecutor
+from tritondse.symbolic_explorator import SymbolicExplorator
+from tritondse.types import Addr
+import tritondse.logging
 
-from tritondse.routines import rtn_atoi
 
-def post_exec_hook(se: SymbolicExecutor, state: ProcessState):
-    print(f"seed:{se.seed.hash} ({repr(se.seed.content)})   [exitcode:{se.exitcode}]")
+logging.basicConfig(level=logging.DEBUG)
+tritondse.logging.enable(level=logging.DEBUG)
 
-def hook_fread(exec: SymbolicExecutor, pstate: ProcessState, routine: str, addr: int):
-    # We hook fread to symbolize what is being read
-    arg = pstate.get_argument_value(0)
-    sizeof = pstate.get_argument_value(2)
-    exec.inject_symbolic_input(arg, exec.seed)
-    print("Symbolizing {} bytes at {}".format(hex(sizeof), hex(arg)))
-    s = pstate.memory.read_string(arg)
-    print(f"fread: {s}")
-    return 0
 
-def hook_sscanf4(exec: SymbolicExecutor, pstate: ProcessState, routine: str, addr: int):
+def trace_inst(se: SymbolicExecutor, pstate: ProcessState, inst: Instruction):
+    logging.debug(f"[tid:{inst.getThreadId()}] 0x{inst.getAddress():x}: {inst.getDisassembly()}")
+
+
+def post_exec_hook(se: SymbolicExecutor, pstate: ProcessState):
+    logging.debug(f"seed:{se.seed.hash} ({repr(se.seed.content)})   [exitcode:{se.exitcode}]")
+
+
+def hook_sscanf4(se: SymbolicExecutor, pstate: ProcessState, rtn_name: str, address: Addr):
     # sscanf(buffer, "%d", &j) is treated as j = atoi(buffer)
     ast = pstate.actx
-    addr_j = pstate.get_argument_value(2)
-    arg = pstate.get_argument_value(0)
-    int_str = pstate.memory.read_string(arg)
 
-    cells = {i: pstate.read_symbolic_memory_byte(arg+i).getAst() for i in range(10)}
+    str_addr = pstate.get_argument_value(0)
+    int_addr = pstate.get_argument_value(2)
+
+    fmt_str = pstate.memory.read_string(str_addr)
+
+    cells = {i: pstate.read_symbolic_memory_byte(str_addr+i).getAst() for i in range(10)}
 
     def multiply(ast, cells, index):
         n = ast.bv(0, 32)
@@ -32,70 +44,76 @@ def hook_sscanf4(exec: SymbolicExecutor, pstate: ProcessState, routine: str, add
             n = n * 10 + (ast.zx(24, cells[i]) - 0x30)
         return n
 
-    res = ast.ite(
-              ast.lnot(ast.land([cells[0] >= 0x30, cells[0] <= 0x39])),
-              multiply(ast, cells, 0),
-              ast.ite(
-                  ast.lnot(ast.land([cells[1] >= 0x30, cells[1] <= 0x39])),
-                  multiply(ast, cells, 1),
-                  ast.ite(
-                      ast.lnot(ast.land([cells[2] >= 0x30, cells[2] <= 0x39])),
-                      multiply(ast, cells, 2),
-                      ast.ite(
-                          ast.lnot(ast.land([cells[3] >= 0x30, cells[3] <= 0x39])),
-                          multiply(ast, cells, 3),
-                          ast.ite(
-                              ast.lnot(ast.land([cells[4] >= 0x30, cells[4] <= 0x39])),
-                              multiply(ast, cells, 4),
-                              ast.ite(
-                                  ast.lnot(ast.land([cells[5] >= 0x30, cells[5] <= 0x39])),
-                                  multiply(ast, cells, 5),
-                                  ast.ite(
-                                      ast.lnot(ast.land([cells[6] >= 0x30, cells[6] <= 0x39])),
-                                      multiply(ast, cells, 6),
-                                      ast.ite(
-                                          ast.lnot(ast.land([cells[7] >= 0x30, cells[7] <= 0x39])),
-                                          multiply(ast, cells, 7),
-                                          ast.ite(
-                                              ast.lnot(ast.land([cells[8] >= 0x30, cells[8] <= 0x39])),
-                                              multiply(ast, cells, 8),
-                                              ast.ite(
-                                                  ast.lnot(ast.land([cells[9] >= 0x30, cells[9] <= 0x39])),
-                                                  multiply(ast, cells, 9),
-                                                  multiply(ast, cells, 9)
-                                              )
-                                          )
-                                      )
-                                  )
-                              )
-                          )
-                      )
-                  )
-              )
-          )
-    res = ast.sx(32, res)
+    int_ast = ast.ite(
+                ast.lnot(ast.land([cells[0] >= 0x30, cells[0] <= 0x39])),
+                multiply(ast, cells, 0),
+                ast.ite(
+                    ast.lnot(ast.land([cells[1] >= 0x30, cells[1] <= 0x39])),
+                    multiply(ast, cells, 1),
+                    ast.ite(
+                        ast.lnot(ast.land([cells[2] >= 0x30, cells[2] <= 0x39])),
+                        multiply(ast, cells, 2),
+                        ast.ite(
+                            ast.lnot(ast.land([cells[3] >= 0x30, cells[3] <= 0x39])),
+                            multiply(ast, cells, 3),
+                            ast.ite(
+                                ast.lnot(ast.land([cells[4] >= 0x30, cells[4] <= 0x39])),
+                                multiply(ast, cells, 4),
+                                ast.ite(
+                                    ast.lnot(ast.land([cells[5] >= 0x30, cells[5] <= 0x39])),
+                                    multiply(ast, cells, 5),
+                                    ast.ite(
+                                        ast.lnot(ast.land([cells[6] >= 0x30, cells[6] <= 0x39])),
+                                        multiply(ast, cells, 6),
+                                        ast.ite(
+                                            ast.lnot(ast.land([cells[7] >= 0x30, cells[7] <= 0x39])),
+                                            multiply(ast, cells, 7),
+                                            ast.ite(
+                                                ast.lnot(ast.land([cells[8] >= 0x30, cells[8] <= 0x39])),
+                                                multiply(ast, cells, 8),
+                                                ast.ite(
+                                                    ast.lnot(ast.land([cells[9] >= 0x30, cells[9] <= 0x39])),
+                                                    multiply(ast, cells, 9),
+                                                    multiply(ast, cells, 9)
+                                                )
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+    int_ast = ast.sx(32, int_ast)
 
-    pstate.write_symbolic_memory_int(addr_j, 8, res)
+    pstate.write_symbolic_memory_int(int_addr, 8, int_ast)
 
     try:
-        i = int(int_str)
-        constraint = res == i
-        pstate.push_constraint(constraint)
-    except:
-        print("Failed to convert to int")
+        i = int(fmt_str)
 
-    return res
+        pstate.push_constraint(int_ast == i)
 
-p = Program("./1")
-dse = SymbolicExplorator(Config(\
-        skip_unsupported_import=True,\
-        seed_format=SeedFormat.COMPOSITE), p)
+        rv = 1
+    except ValueError:
+        logging.debug("Failed to convert to int!")
+        rv = 0
 
-dse.add_input_seed(Seed(CompositeData(files={"stdin": b"AZERZAER", "tmp.covpro": b"AZERAEZR"})))
+    return rv
+
+
+prog = Program("./bin/1")
+
+config = Config(skip_unsupported_import=True,
+                seed_format=SeedFormat.COMPOSITE)
+
+dse = SymbolicExplorator(config, prog)
+
+dse.add_input_seed(Seed(CompositeData(files={"tmp.covpro": b"AZERAEZR"})))
 
 dse.callback_manager.register_post_execution_callback(post_exec_hook)
 dse.callback_manager.register_probe(NullDerefSanitizer())
-#dse.callback_manager.register_post_imported_routine_callback("fread", hook_fread)
 dse.callback_manager.register_pre_imported_routine_callback("__isoc99_sscanf", hook_sscanf4)
+# dse.callback_manager.register_pre_instruction_callback(trace_inst)
 
 dse.explore()
