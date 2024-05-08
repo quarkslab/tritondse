e8023e66219939c6d824fb935aab29d1e6dde783
Christian Heitman <cheitman@quarkslab.com>
2024-05-08 14:22:52 -0300

    Refactor solve0 script

--- doc/practicals/solutions_toy_examples/solve0.py
+++ doc/practicals/solutions_toy_examples/solve0.py
@@ -1,22 +1,41 @@
-from tritondse import ProbeInterface, SymbolicExecutor, Config, Program, SymbolicExplorator, ProcessState, CbType, SeedStatus, Seed, SeedFormat, CompositeData
-from tritondse.types import Addr, SolverStatus, Architecture
-from tritondse.sanitizers import NullDerefSanitizer
+import logging
+
 from triton import Instruction
 
-def trace_inst(exec: SymbolicExecutor, pstate: ProcessState, inst: Instruction):
-    print(f"[tid:{inst.getThreadId()}] 0x{inst.getAddress():x}: {inst.getDisassembly()}")
+from tritondse.config import Config
+from tritondse.loaders.program import Program
+from tritondse.process_state import ProcessState
+from tritondse.sanitizers import NullDerefSanitizer
+from tritondse.seed import CompositeData
+from tritondse.seed import Seed
+from tritondse.seed import SeedFormat
+from tritondse.symbolic_executor import SymbolicExecutor
+from tritondse.symbolic_explorator import SymbolicExplorator
+import tritondse.logging
 
-def post_exec_hook(se: SymbolicExecutor, state: ProcessState):
-    print(f"seed:{se.seed.hash} ({repr(se.seed.content)})")
 
-p = Program("./7")
-dse = SymbolicExplorator(Config(\
-        skip_unsupported_import=True,\
-        seed_format=SeedFormat.COMPOSITE), p)
+logging.basicConfig(level=logging.DEBUG)
+tritondse.logging.enable(level=logging.DEBUG)
 
-dse.add_input_seed(Seed(CompositeData(argv=[b"./7", b"XXXX"], files={"stdin": b"ZZZZ"})))
+
+def trace_inst(se: SymbolicExecutor, pstate: ProcessState, inst: Instruction):
+    logging.debug(f"[tid:{inst.getThreadId()}] 0x{inst.getAddress():x}: {inst.getDisassembly()}")
+
+
+def post_exec_hook(se: SymbolicExecutor, pstate: ProcessState):
+    logging.debug(f"seed:{se.seed.hash} ({repr(se.seed.content)})")
+
+
+prog = Program("./bin/0")
+
+config = Config(skip_unsupported_import=True,
+                seed_format=SeedFormat.COMPOSITE)
+
+dse = SymbolicExplorator(config, prog)
+
+dse.add_input_seed(Seed(CompositeData(argv=[b"./bin/0", b"XXXX"], files={"stdin": b"ZZZZ"})))
 dse.callback_manager.register_probe(NullDerefSanitizer())
 dse.callback_manager.register_post_execution_callback(post_exec_hook)
-#dse.callback_manager.register_post_instruction_callback(trace_inst)
+# dse.callback_manager.register_post_instruction_callback(trace_inst)
 
 dse.explore()
